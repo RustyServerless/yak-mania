67fa73fc1d04ff0e15b49ca808ade476167521e3
Jérémie Rodon <jeremie.rodon@gmail.com>
2026-09-21 20:19:11 +0200

    chore: Update Nix env

--- nix/flake.nix
+++ nix/flake.nix
@@ -7,7 +7,6 @@
       url = "github:oxalica/rust-overlay";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
       };
     };
   };
@@ -37,8 +36,7 @@
       in
         with pkgs; {
           devShells.default = mkShell {
-            buildInputs = [rustToolchain dependencies];
-            RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            buildInputs = [rustToolchain] ++ dependencies;
           };
         }
     );
